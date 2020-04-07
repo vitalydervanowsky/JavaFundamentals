4689c6a9af580c196118c18fb2c8651f02679b15
Vitali Dzervanouski <vitalydervanowsky@gmail.com>
2020-04-07 14:55:12 +0300

    feat: add Task 1 (Hello class)

---
+++ src/by/mzkt/Hello.java
@@ -0,0 +1,11 @@
+package by.mzkt;
+
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String helloName = sc.nextLine();
+        System.out.println("Hello " + helloName);
+    }
+}
