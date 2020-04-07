1f89feece3712a02bb1c9fe193fc900f72592e44
Vitali Dzervanouski <vitalydervanowsky@gmail.com>
2020-04-07 15:21:16 +0300

    feat: add Task 5 (Months class)

---
+++ src/by/mzkt/Months.java
@@ -0,0 +1,64 @@
+package by.mzkt;
+
+import java.util.Scanner;
+
+public class Months {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите номер месяца (1 - 12): ");
+        int month = sc.nextInt();
+        switch (month) {
+            case 1: {
+                System.out.println("Январь");
+                break;
+            }
+            case 2: {
+                System.out.println("Февраль");
+                break;
+            }
+            case 3: {
+                System.out.println("Март");
+                break;
+            }
+            case 4: {
+                System.out.println("Апрель");
+                break;
+            }
+            case 5: {
+                System.out.println("Май");
+                break;
+            }
+            case 6: {
+                System.out.println("Июнь");
+                break;
+            }
+            case 7: {
+                System.out.println("Июль");
+                break;
+            }
+            case 8: {
+                System.out.println("Август");
+                break;
+            }
+            case 9: {
+                System.out.println("Сентябрь");
+                break;
+            }
+            case 10: {
+                System.out.println("Октябрь");
+                break;
+            }
+            case 11: {
+                System.out.println("Ноябрь");
+                break;
+            }
+            case 12: {
+                System.out.println("Декабрь");
+                break;
+            }
+            default: {
+                System.out.println("Вы ввели некорректное значение");
+            }
+        }
+    }
+}
